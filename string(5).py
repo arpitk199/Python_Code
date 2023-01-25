090962e6e438590b392ae235f04856b5c18e66e0
arpitk199 <kumararpi199@gmail.com>
2023-01-25 09:53:59 +0530

    first commit

---
+++ string(5).py
@@ -0,0 +1,10 @@
+#Important Point about String
+# * String Construct is str()
+# a=str()
+# print(a)
+
+a="Amit"
+print(a)
+# * String is a set of sequence of element
+# * string support indexing
+# String support slicing
